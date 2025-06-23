26d60beace41446b6b1b47997e92935c892f5549
Daniel Polo <od.polo@gmail.com>
2025-06-23 06:10:59 -0600

    refactor: reorganize tags policies in migration files
    - Removed existing tag policies from the create_policies migration.
    - Added tag policies in the new add_tags migration to ensure proper management of user tags.

--- supabase/migrations/20250604000000_add_tags.sql
+++ supabase/migrations/20250604000000_add_tags.sql
@@ -8,3 +8,22 @@ create table public.tags (
 
 alter table public.tags
   add constraint tags_user_id_fkey foreign key (user_id) references auth.users(id) on delete cascade;
+
+create policy "tags_policy"
+    on tags
+    for select using ( (select auth.uid()) = user_id );
+create policy "tags_insert_policy"
+    on tags
+    for insert
+    to authenticated
+    with check (true);
+create policy "tags_update_policy"
+    on tags
+    for update
+    to authenticated
+    using ( (select auth.uid()) = user_id )
+    with check ( (select auth.uid()) = user_id );
+create policy "tags_delete_policy"
+    on tags
+    for delete using ( (select auth.uid()) = user_id );
+
