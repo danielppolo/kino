693e2308175f73f943c8359870d4720e3f40d0b6
Daniel Polo <od.polo@gmail.com>
2025-07-08 19:55:57 -0600

    Refactor NavUser component to comment out Avatar and email display
    - Commented out the Avatar component and the email display within the NavUser component, streamlining the user interface and potentially preparing for future updates or design changes.

--- components/shared/nav-user.tsx
+++ components/shared/nav-user.tsx
@@ -76,15 +76,15 @@ export function NavUser() {
                 size="lg"
                 className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
               >
-                <Avatar className="h-8 w-8 rounded-lg">
+                {/* <Avatar className="h-8 w-8 rounded-lg">
                   <AvatarImage src={user.avatar} alt={user.name} />
                   <AvatarFallback className="rounded-lg">
                     {user.name.charAt(0)}
                   </AvatarFallback>
-                </Avatar>
+                </Avatar> */}
                 <div className="grid flex-1 text-left text-sm leading-tight">
                   <span className="truncate font-medium">{user.name}</span>
-                  <span className="truncate text-xs">{user.email}</span>
+                  {/* <span className="truncate text-xs">{user.email}</span> */}
                 </div>
                 <ChevronsUpDown className="ml-auto size-4" />
               </SidebarMenuButton>
